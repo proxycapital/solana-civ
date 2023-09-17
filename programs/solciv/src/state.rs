1f4f18822845d1f3bdd5bacfbdfc78ccb74593cf
dwrx <dwrx@miso.one>
2023-09-17 22:03:19 +0200

    Add states for Units, their types and Position on global map

--- programs/solciv/src/state.rs
+++ programs/solciv/src/state.rs
@@ -5,6 +5,46 @@ use solana_program::{
 };
 
 #[derive(BorshSerialize, BorshDeserialize)]
+pub struct Position {
+    pub x: u32,
+    pub y: u32,
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct Unit {
+    pub id: u32,
+    pub unit_type: UnitType,
+    pub health: u32,
+    pub attack: u32,
+    pub movement_range: u8,
+    pub visibility_range: u8, // for the "fog of war"
+    pub remaining_actions: u8, // e.g. builder can move and build in the same turn, but number of total builds is limited per his lifetime
+    pub position: Position,
+    pub is_alive: bool, // not sure if needed, but unit is not necessary dead, should be removed also when remaining_actions == 0 (settler, builder)
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub enum UnitType {
+    Settler,
+    Builder,
+    Warrior,
+    Swordsman,
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct UnitSeq {
+    pub next_unit_id: u32, // to be used as a unique id for each new unit
+    // @todo: add game session identifier ?
+}
+
+// @TODO: merge player state with balances etc?
+// #[derive(BorshSerialize, BorshDeserialize)]
+// pub struct PlayerState {
+//     pub is_initialized: bool,
+//     pub units: Vec<Unit>,
+// }
+
+#[derive(BorshSerialize, BorshDeserialize)]
 pub struct GameAccountState {
     pub is_initialized: bool,
     pub map: [u8; 400],
