e749d321f87d8874bb02b0b903f020264535360f
dwrx <dwrx@miso.one>
2023-09-24 17:03:45 +0200

    Add instructions: initialize game, initialize player

--- programs/solciv/programs/solciv/src/lib.rs
+++ programs/solciv/programs/solciv/src/lib.rs
@@ -2,14 +2,162 @@ use anchor_lang::prelude::*;
 
 declare_id!("GoiXQMoEhhLM8MSbfUFhHz4punJqXNHEQh6ysegmuHJz");
 
+const MAX_UNITS: u8 = 10;
+
 #[program]
 pub mod solciv {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize_game(ctx: Context<InitializeGame>, map: [u8; 400]) -> Result<()> {
+        ctx.accounts.game.player = ctx.accounts.player.key().clone();
+        ctx.accounts.game.map = map;
+    
+        msg!("Game created!");
+    
+        Ok(())
+    }
+
+    pub fn initialize_player(ctx: Context<InitializePlayer>) -> Result<()> {
+        ctx.accounts.player_account.game = ctx.accounts.game.key().clone();
+        ctx.accounts.player_account.player = ctx.accounts.player.key().clone();
+        ctx.accounts.player_account.points = 0;
+        ctx.accounts.player_account.resources = Resources {
+            gold: 0,
+            food: 10,
+            wood: 5,
+            stone: 0,
+            iron: 0,
+        };
+        ctx.accounts.player_account.units = vec![
+            Unit {
+                unit_id: 0,
+                player: ctx.accounts.player.key().clone(),
+                game: ctx.accounts.game.key().clone(),
+                unit_type: UnitType::Settler,
+                x: 2,
+                y: 2,
+                attack: 0,
+                health: 100,
+                movement_range: 2,
+                remaining_actions: 1,
+                is_alive: true,
+            },
+            Unit {
+                unit_id: 1,
+                player: ctx.accounts.player.key().clone(),
+                game: ctx.accounts.game.key().clone(),
+                unit_type: UnitType::Builder,
+                x: 3,
+                y: 2,
+                attack: 14,
+                health: 100,
+                movement_range: 2,
+                remaining_actions: 1,
+                is_alive: true,
+            },
+            Unit {
+                unit_id: 2,
+                player: ctx.accounts.player.key().clone(),
+                game: ctx.accounts.game.key().clone(),
+                unit_type: UnitType::Warrior,
+                x: 2,
+                y: 3,
+                attack: 14,
+                health: 100,
+                movement_range: 2,
+                remaining_actions: 1,
+                is_alive: true,
+            }
+        ];
+    
+        msg!("Player created!");
+    
         Ok(())
     }
 }
 
+#[account]
+pub struct Game {
+    pub player: Pubkey,
+    pub map: [u8; 400],
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeGame<'info> {
+    #[account(
+        init, 
+        seeds=[b"GAME", player.key().as_ref()],
+        bump,
+        payer = player, 
+        space = std::mem::size_of::<Game>()+ 8
+    )]
+    pub game: Account<'info, Game>,
+
+    #[account(mut)]
+    pub player: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Player {
+    pub game: Pubkey,
+    pub player: Pubkey,
+    pub points: u64,
+    pub units: Vec<Unit>,
+    pub resources: Resources,
+}
+
+#[derive(Accounts)]
+pub struct InitializePlayer<'info> {
+    pub game: Account<'info, Game>,
+
+    #[account(
+        init, 
+        seeds=[
+            b"PLAYER", 
+            game.key().as_ref(), 
+            player.key().as_ref()
+        ], 
+        bump, 
+        payer = player, 
+        space = std::mem::size_of::<Player>() + std::mem::size_of::<Unit>() * MAX_UNITS as usize + std::mem::size_of::<Resources>() + 8)
+    ]
+    pub player_account: Box<Account<'info, Player>>,
+
+    #[account(mut)]
+    pub player: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Copy, Clone)]
+pub struct Unit {
+    pub unit_id: u8,
+    pub player: Pubkey,
+    pub game: Pubkey,
+    pub unit_type: UnitType,
+    pub x: u8,
+    pub y: u8,
+    pub attack: u8,
+    pub health: u8,
+    pub movement_range: u8,
+    pub remaining_actions: u8,
+    pub is_alive: bool,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Copy, Clone)]
+pub enum UnitType {
+    Settler,
+    Builder,
+    Warrior,
+    Archer,
+    Swordsman,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Copy, Clone)]
+pub struct Resources {
+    pub gold: u32,
+    pub food: u32,
+    pub wood: u32,
+    pub stone: u32,
+    pub iron: u32,
+}
--- programs/solciv/tests/solciv.ts
+++ programs/solciv/tests/solciv.ts
@@ -1,16 +1,57 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Solciv } from "../target/types/solciv";
+import { expect } from "chai";
 
 describe("solciv", () => {
+
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Solciv as Program<Solciv>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const [gameKey] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("GAME"), provider.publicKey.toBuffer()],
+    program.programId
+  );
+  console.log("Game account address", gameKey.toString());
+
+  const [playerKey] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("PLAYER"), gameKey.toBuffer(), provider.publicKey.toBuffer()],
+    program.programId
+  );
+  console.log("Player account address", playerKey.toString());
+
+  it("Initialize game", async () => {
+    // generate random 20x20 map with tile types from 1 to 9
+    const randomMap = Array.from({length: 400}, () => Math.floor(Math.random() * 9) + 1);
+
+    const accounts = {
+      game: gameKey,
+      player: provider.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    };
+    const tx = await program.methods.initializeGame(randomMap).accounts(accounts).rpc();
+    console.log("Transaction signature", tx);
+
+    const account = await program.account.game.fetch(gameKey);
+
+    expect(account.player.toBase58()).equal(provider.publicKey.toBase58());
+    expect(account.map).deep.equal(randomMap);
+  });
+
+  it("Initialize player with units and balances", async () => {
+    const accounts = {
+      game: gameKey,
+      playerAccount: playerKey,
+      player: provider.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    };
+    const tx = await program.methods.initializePlayer().accounts(accounts).rpc();
+    console.log("Transaction signature", tx);
+
+    const account = await program.account.player.fetch(playerKey);
+    console.log(account);
   });
 });
