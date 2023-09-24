a7663bf9d0c216aeec5e72c84f92aa6bb0659797
dwrx <dwrx@miso.one>
2023-09-24 23:35:01 +0200

    Add instructions to move units and to end turn

--- programs/solciv/programs/solciv/src/lib.rs
+++ programs/solciv/programs/solciv/src/lib.rs
@@ -1,5 +1,8 @@
 use anchor_lang::prelude::*;
 
+pub mod errors;
+use crate::errors::UnitError;
+
 declare_id!("GoiXQMoEhhLM8MSbfUFhHz4punJqXNHEQh6ysegmuHJz");
 
 const MAX_UNITS: u8 = 10;
@@ -10,6 +13,7 @@ pub mod solciv {
 
     pub fn initialize_game(ctx: Context<InitializeGame>, map: [u8; 400]) -> Result<()> {
         ctx.accounts.game.player = ctx.accounts.player.key().clone();
+        ctx.accounts.game.turn = 1;
         ctx.accounts.game.map = map;
     
         msg!("Game created!");
@@ -74,11 +78,70 @@ pub mod solciv {
     
         Ok(())
     }
+
+    // #[access_control(MoveUnit::validate_unit_move(&ctx, unit_id, x, y))]
+    pub fn move_unit(
+        ctx: Context<MoveUnit>,
+        unit_id: u8,
+        x: u8,
+        y: u8,
+    ) -> Result<()> {
+        let unit = ctx.accounts.player_account.units.iter().find(|u| u.unit_id == unit_id).ok_or(UnitError::UnitNotFound)?;
+        
+        // Check if the tile is within the map bounds
+        if x >= 20 || y >= 20 {
+            return err!(UnitError::OutOfMapBounds);
+        }
+
+        // Check if the unit has remaining movement_range points
+        if unit.movement_range == 0 {
+            return err!(UnitError::CannotMove);
+        }
+
+        // Check if the new position is within the movement_range
+        let dist = ((unit.x as i16 - x as i16).abs() + (unit.y as i16 - y as i16).abs()) as u8;
+        msg!("Initial position: ({}, {})", unit.x, unit.y);
+        msg!("New position: ({}, {})", x, y);
+        msg!("Distance: {}", dist);
+        if dist > unit.movement_range {
+            return err!(UnitError::OutOfMovementRange);
+        }
+
+        // Check if the tile is not occupied by another unit
+        if ctx.accounts.player_account.units.iter().any(|u| u.x == x && u.y == y && u.unit_id != unit_id) {
+            return err!(UnitError::TileOccupied);
+        }
+
+        let units = &mut ctx.accounts.player_account.units;
+        
+        // Find the index of the unit with the given unit_id
+        let unit_idx = units.iter().position(|u| u.unit_id == unit_id).ok_or(UnitError::UnitNotFound)?;
+        
+        // Update the coordinates of the unit
+        ctx.accounts.player_account.units[unit_idx].x = x;
+        ctx.accounts.player_account.units[unit_idx].y = y;
+        ctx.accounts.player_account.units[unit_idx].movement_range -= dist;
+
+        Ok(())
+    }
+
+    pub fn end_turn(ctx: Context<EndTurn>) -> Result<()> {
+        // Iterate over all units of the player and reset the movement_range to 2
+        for unit in &mut ctx.accounts.player_account.units.iter_mut() {
+            if unit.is_alive {
+                unit.movement_range = 2;
+            }
+        }
+        // units.retain(|unit| unit.is_alive);
+        ctx.accounts.game.turn += 1;
+        Ok(())
+    }
 }
 
 #[account]
 pub struct Game {
     pub player: Pubkey,
+    pub turn: u32,
     pub map: [u8; 400],
 }
 
@@ -89,7 +152,7 @@ pub struct InitializeGame<'info> {
         seeds=[b"GAME", player.key().as_ref()],
         bump,
         payer = player, 
-        space = std::mem::size_of::<Game>()+ 8
+        space = std::mem::size_of::<Game>() + 8
     )]
     pub game: Account<'info, Game>,
 
@@ -160,4 +223,22 @@ pub struct Resources {
     pub wood: u32,
     pub stone: u32,
     pub iron: u32,
+}
+
+#[derive(Accounts)]
+pub struct MoveUnit<'info> {
+    #[account(mut)]
+    player_account: Account<'info, Player>,
+    #[account(mut)]
+    pub player: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct EndTurn<'info> {
+    #[account(mut)]
+    game: Account<'info, Game>,
+    #[account(mut)]
+    player_account: Account<'info, Player>,
+    #[account(mut)]
+    pub player: Signer<'info>,
 }
