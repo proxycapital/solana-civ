3977702580183e44a615b0b8754e3b1d7521bf9f
dwrx <dwrx@miso.one>
2023-09-25 19:28:40 +0200

    Add instruction to found a city

--- programs/solciv/programs/solciv/src/lib.rs
+++ programs/solciv/programs/solciv/src/lib.rs
@@ -1,11 +1,13 @@
 use anchor_lang::prelude::*;
 
 pub mod errors;
-use crate::errors::UnitError;
+use crate::errors::{UnitError, BuildingError};
 
 declare_id!("GoiXQMoEhhLM8MSbfUFhHz4punJqXNHEQh6ysegmuHJz");
 
-const MAX_UNITS: u8 = 10;
+const MAX_UNITS: u8 = 20;
+const MAX_CITIES: u8 = 20;
+const MAX_BUILDINGS: u8 = 20;
 
 #[program]
 pub mod solciv {
@@ -25,6 +27,8 @@ pub mod solciv {
         ctx.accounts.player_account.game = ctx.accounts.game.key().clone();
         ctx.accounts.player_account.player = ctx.accounts.player.key().clone();
         ctx.accounts.player_account.points = 0;
+        ctx.accounts.player_account.next_city_id = 0;
+        ctx.accounts.player_account.next_unit_id = 0;
         ctx.accounts.player_account.resources = Resources {
             gold: 0,
             food: 10,
@@ -73,6 +77,7 @@ pub mod solciv {
                 is_alive: true,
             }
         ];
+        ctx.accounts.player_account.next_unit_id = 3;
     
         msg!("Player created!");
     
@@ -82,7 +87,7 @@ pub mod solciv {
     // #[access_control(MoveUnit::validate_unit_move(&ctx, unit_id, x, y))]
     pub fn move_unit(
         ctx: Context<MoveUnit>,
-        unit_id: u8,
+        unit_id: u32,
         x: u8,
         y: u8,
     ) -> Result<()> {
@@ -121,7 +126,53 @@ pub mod solciv {
         ctx.accounts.player_account.units[unit_idx].x = x;
         ctx.accounts.player_account.units[unit_idx].y = y;
         ctx.accounts.player_account.units[unit_idx].movement_range -= dist;
+        
+        Ok(())
+    }
 
+    pub fn found_city(ctx: Context<FoundCity>, x: u8, y: u8, unit_id: u32) -> Result<()> {
+        // 1. Validate if the unit with `unit_id` is a settler and is at `x` and `y`.
+        let unit_idx = ctx.accounts.player_account.units.iter().position(|u| u.unit_id == unit_id).ok_or(UnitError::UnitNotFound)?;
+        let unit = &ctx.accounts.player_account.units[unit_idx];
+        if unit.unit_type != UnitType::Settler {
+            return err!(UnitError::InvalidUnitType);
+        }
+        if (unit.x, unit.y) != (x, y) {
+            return err!(UnitError::UnitWrongPosition);
+        }
+        
+        // 2. Check if there is already a city at `x` and `y`.
+        let is_occupied = ctx.accounts.player_account.cities.iter().any(|city| city.x == x && city.y == y);
+        if is_occupied {
+            return err!(BuildingError::TileOccupied);
+        }
+
+        // 3. Initialize the new City.
+        let new_city = City {
+            city_id: ctx.accounts.player_account.next_city_id,
+            player: ctx.accounts.player_account.player,
+            game: ctx.accounts.game.key(),
+            x,
+            y,
+            health: 100,
+            defence: 0,
+            population: 1,
+            gold_yield: 2,
+            food_yield: 2,
+            production_yield: 2,
+            science_yield: 1,
+            buildings: vec![],
+        };
+        ctx.accounts.player_account.cities.push(new_city);
+        
+        // 4. Remove the settler unit used to found the city.
+        ctx.accounts.player_account.units.remove(unit_idx);
+        
+        // 5. Update the next_city_id in the player account.
+        ctx.accounts.player_account.next_city_id = ctx.accounts.player_account.next_city_id.checked_add(1).unwrap();
+        
+        msg!("Founded new city!");
+        
         Ok(())
     }
 
@@ -132,6 +183,16 @@ pub mod solciv {
                 unit.movement_range = 2;
             }
         }
+        let mut gold = 0;
+        let mut food = 0;
+        for city in &mut ctx.accounts.player_account.cities {
+            gold += city.gold_yield;
+            food += city.food_yield;
+        }
+
+        ctx.accounts.player_account.resources.gold = ctx.accounts.player_account.resources.gold.checked_add(gold).unwrap();
+        ctx.accounts.player_account.resources.food = ctx.accounts.player_account.resources.food.checked_add(food).unwrap();
+
         // units.retain(|unit| unit.is_alive);
         ctx.accounts.game.turn += 1;
         Ok(())
@@ -165,9 +226,12 @@ pub struct InitializeGame<'info> {
 pub struct Player {
     pub game: Pubkey,
     pub player: Pubkey,
-    pub points: u64,
+    pub points: u32,
+    pub cities: Vec<City>,
     pub units: Vec<Unit>,
     pub resources: Resources,
+    pub next_city_id: u32,
+    pub next_unit_id: u32,
 }
 
 #[derive(Accounts)]
@@ -183,7 +247,11 @@ pub struct InitializePlayer<'info> {
         ], 
         bump, 
         payer = player, 
-        space = std::mem::size_of::<Player>() + std::mem::size_of::<Unit>() * MAX_UNITS as usize + std::mem::size_of::<Resources>() + 8)
+        space = std::mem::size_of::<Player>() +
+            std::mem::size_of::<Unit>() * MAX_UNITS as usize +
+            std::mem::size_of::<City>() * MAX_CITIES as usize +
+            std::mem::size_of::<BuildingType>() * MAX_BUILDINGS as usize +
+            std::mem::size_of::<Resources>() + 8)
     ]
     pub player_account: Box<Account<'info, Player>>,
 
@@ -192,9 +260,55 @@ pub struct InitializePlayer<'info> {
     pub system_program: Program<'info, System>,
 }
 
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Copy)]
+pub enum BuildingType {
+    Barracks,
+    Wall,
+    Market,
+    Library,
+    School,
+    University,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone)]
+pub struct City {
+    pub city_id: u32,
+    pub player: Pubkey,
+    pub game: Pubkey,
+    pub x: u8,
+    pub y: u8,
+    pub health: u32,
+    pub defence: u32,
+    pub population: u32,
+    pub gold_yield: u32,
+    pub food_yield: u32,
+    pub production_yield: u32,
+    pub science_yield: u32,
+    pub buildings: Vec<BuildingType>,
+}
+
+#[derive(Accounts)]
+pub struct FoundCity<'info> {
+    #[account(mut)]
+    game: Account<'info, Game>,
+    #[account(mut)]
+    player_account: Account<'info, Player>,
+    // #[account(
+    //     init, 
+    //     payer = player, 
+    //     seeds=[b"CITY", game.key().as_ref(), player.key().as_ref(), player_account.next_city_id.to_le_bytes().as_ref()], 
+    //     bump, 
+    //     space = std::mem::size_of::<City>() + 8
+    // )]
+    // pub city: Account<'info, City>,
+    #[account(mut)]
+    pub player: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
 #[derive(AnchorSerialize, AnchorDeserialize, Copy, Clone)]
 pub struct Unit {
-    pub unit_id: u8,
+    pub unit_id: u32,
     pub player: Pubkey,
     pub game: Pubkey,
     pub unit_type: UnitType,
@@ -207,7 +321,7 @@ pub struct Unit {
     pub is_alive: bool,
 }
 
-#[derive(AnchorSerialize, AnchorDeserialize, Copy, Clone)]
+#[derive(AnchorSerialize, AnchorDeserialize, Copy, Clone, PartialEq)]
 pub enum UnitType {
     Settler,
     Builder,
