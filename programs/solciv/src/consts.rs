9efdc93d4ec89f33ade0285dfddfb41a5bd39a03
orso <orso@metaversium.io>
2024-01-14 14:05:05 +0200

    Fix difficulty order

--- programs/solciv/src/consts.rs
+++ programs/solciv/src/consts.rs
@@ -11,4 +11,4 @@ pub const GEMS_PER_CITY_DESTROYED: u8 = 50;
 pub const EXP_THRESHOLDS: [u8; 3] = [10, 30, 45];
 pub const EXP_PER_ATTACK: u8 = 3;
 
-pub const SPAWN_INTERVAL: [u8; 3] = [10, 15, 20];
+pub const SPAWN_INTERVAL: [u8; 3] = [20, 15, 10];
